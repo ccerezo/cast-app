88a0dc965cf0a2defb59f5f2699f23c9ba9e7f53
ccerezo <ccerezo90@gmail.com>
2021-07-05 12:47:41 -0500

    tamaño 16 factura

--- resources/views/reportes/factura.blade.php
+++ resources/views/reportes/factura.blade.php
@@ -5,6 +5,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <style type="text/css">
+    body{
+        font-size: 16px;
+    }
     .container{
         width: auto;
         margin-top: 240px;
