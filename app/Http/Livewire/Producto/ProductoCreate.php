aedfa2e3f6d75eb2521776740debc3f322597a9e
ccerezo <ccerezo90@gmail.com>
2021-08-19 08:48:35 -0500

    productos
    mostrar tallas al seleccionar lineas

--- app/Http/Livewire/Producto/ProductoCreate.php
+++ app/Http/Livewire/Producto/ProductoCreate.php
@@ -19,20 +19,23 @@ class ProductoCreate extends Component
     public function render()
     {
         $lineas = Linea::orderBy('nombre', 'asc')->pluck('nombre','id');
+        $lineas->prepend('--Seleccione--', 0);
         $categorias = Categoria::orderBy('nombre', 'asc')->pluck('nombre','id');
         $bodegas = Bodega::pluck('nombre','id');
         $marcas = Marca::orderBy('nombre', 'asc')->pluck('nombre','id');
         $modelos = Modelo::orderBy('nombre', 'asc')->pluck('nombre','id');
         $colorTmp = Color::orderBy('nombre', 'asc')->pluck('nombre','id');
         $colores = Color::where('id','=',$this->colorSelected)->orderBy('nombre', 'asc')->get();
-        $lineas_tallaje = Linea::pluck('id')->first();
+        //$lineas_tallaje = Linea::pluck('id')->first();
         $descuentos = ['0'=> '0%','5'=> '5%','10' =>'10%','15' =>'15%','20' =>'20%','25' =>'25%','30' =>'30%'];
         if(isset($this->lineaSelected) && $this->lineaSelected > 0){
             $tallajes = Tallaje::where('linea_id', '=', $this->lineaSelected)->pluck('talla_id');
             $tallas = Talla::whereIn('id', $tallajes)->get();
         } else {
-            $tallajes = Tallaje::where('linea_id', '=', $lineas_tallaje)->pluck('talla_id');
-            $tallas = Talla::whereIn('id', $tallajes)->get();
+            //$tallajes = Tallaje::where('linea_id', '=', $lineas_tallaje)->pluck('talla_id');
+            //$tallas = Talla::whereIn('id', $tallajes)->get();
+            $tallajes = [];
+            $tallas = [];
         }
 
         return view('livewire.producto.producto-create', compact('lineas','categorias','tallas','bodegas','marcas','modelos','colores','colorTmp','descuentos'));
